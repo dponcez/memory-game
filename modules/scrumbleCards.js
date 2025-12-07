e96e2a92c5c1d86fd0eef9dddafed9955abea6a1
Damian Ponce <dponcez_git@hotmail.com>
2025-12-07 00:19:15 -0500

    feat: implement scrumbleCards function to shuffle card array

---
+++ modules/scrumbleCards.js
@@ -0,0 +1,16 @@
+export const scrumbleCards = (cards) => {
+  const sliceCards = cards.slice();
+  const length = sliceCards.length;
+
+  while (length > 0) {
+    const randomizeCards = Math.floor(Math.random() * length);
+    length--;
+
+    [sliceCards[length], 
+      sliceCards[randomizeCards]] = 
+    [sliceCards[randomizeCards], 
+      sliceCards[length]];
+  }
+
+  return sliceCards;
+};
