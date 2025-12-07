d57dd3379eef468dc6335c6252aa1b0ad2da4f4c
Damian Ponce <dponcez_git@hotmail.com>
2025-12-07 00:31:21 -0500

    feat: add initial script to create game table with icons

---
+++ js/script.js
@@ -0,0 +1,11 @@
+import { createTable } from '../modules/createTable.js';
+
+const icons = [
+  '🍎', '🍌', '🍇', '🍉',
+  '🍎', '🍌', '🍇', '🍉',
+  '🎁', '🏀', '🐶', '❤️',
+  '🎁', '🏀', '🐶', '❤️'
+
+];
+
+createTable(icons)
