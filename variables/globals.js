8268d2493f76e5914a510016cfc764cfede68fcc
Damian Ponce <dponcez_git@hotmail.com>
2025-12-15 23:07:33 -0500

    feat: add variable as object

---
+++ variables/globals.js
@@ -0,0 +1,8 @@
+import { selector } from "../_fns/custom_functions.js";
+
+export const states = {
+  startGameBtn: selector('[data-start-btn]'),
+  screen: selector('[data-screen]'),
+  timer_text: selector('[data-timer]'),
+  score_text: selector('[data-score]')
+}
