7bf7b076d25f7b5555bfcbe7785a52d9454505dd
Damian Ponce <dponcez_git@hotmail.com>
2025-12-18 21:40:07 -0500

    refactor: remove unused selector and use states for tableBody

--- modules/createTable.js
+++ modules/createTable.js
@@ -1,10 +1,11 @@
-import { selector, selectorAll, handler } from "../_fns/custom_functions.js";
+import { selectorAll, handler } from "../_fns/custom_functions.js";
 import { scrumbleCards } from "./scrumbleCards.js";
 import { handleFlipCards } from './flipCards.js';
+import { states } from '../variables/globals.js';
 
 export const createTable = (icons) => {
+  const { tableBody } = states;
   const scrumble_cards = scrumbleCards(icons);
-  const tableBody = selector("[data-main]");
 
   scrumble_cards.forEach((card, index) => {
     tableBody.innerHTML += `
