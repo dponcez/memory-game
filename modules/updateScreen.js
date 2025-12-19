f17aaa5a1c74c7262bec5356b38fecda0cd0e4ca
Damian Ponce <dponcez_git@hotmail.com>
2025-12-18 20:18:32 -0500

    refactor: rename updateScreen  to updateStates module

--- modules/updateScreen.js
+++
@@ -1,4 +0,0 @@
-import { states } from '../variables/globals.js';
-
-const { timer_text } = states;
-export const updateScreen = (time) => timer_text.textContent = `${time}`;
---
+++ modules/updateStates.js
@@ -0,0 +1,6 @@
+import { states } from '../variables/globals.js';
+
+const { timer_text, score_text } = states;
+export const updateScreen = (time) => timer_text.textContent = `${time}`;
+
+export const updateScore = (score) => score_text.textContent = `${score}`;
