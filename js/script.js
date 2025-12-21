95f99f75694f3549038838f11510e530f0e9cf04
Damian Ponce <dponcez_git@hotmail.com>
2025-12-20 20:48:20 -0500

    refactor: replace hardcoded icons with cardIcons module for dynamic icon generation

--- js/script.js
+++ js/script.js
@@ -2,15 +2,10 @@ import { createTable } from '../modules/createTable.js';
 import { handler } from '../_fns/custom_functions.js';
 import { states } from '../variables/globals.js';
 import { startGame } from '../modules/startGame.js';
+import { cardIcons } from '../modules/cardIcons.js';
 
 const { startGameBtn } = states;
-const icons = [
-  '🍎', '🍌', '🍇', '🍉',
-  '🍎', '🍌', '🍇', '🍉',
-  '🎁', '🏀', '🐶', '❤️',
-  '🎁', '🏀', '🐶', '❤️'
-
-];
+const icons = cardIcons();
 
 handler(startGameBtn, 'click', startGame);
 createTable(icons)
